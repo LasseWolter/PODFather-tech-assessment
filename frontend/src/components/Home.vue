ebb97a742d5bb74e3fe1736af3afda8d78060854
Lasse Wolter <wolter.lasse@gmail.com>
2022-06-25 22:04:37 +0100

    feat(FE): simple vue frontend displaying raw jobs

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <li v-for="job in jobs" v-bind:key="job.id">
+    <p>{{ job }}</p>
+  </li>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get("http://localhost:8000/jobs");
+        // JSON responses are automatically parsed.
+        this.jobs = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
